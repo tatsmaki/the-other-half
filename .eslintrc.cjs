814b4ee761add3598608d0f72f362790ae5cb395
Rostislav Harlanov <rastsislau.kharlanau@innowise-group.com>
2024-07-03 00:01:38 +0200

    build: configure eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended-type-checked"],
+  ignorePatterns: ["node_modules", "dist", ".eslintrc.cjs", "vite.config.ts"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json"],
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    "@typescript-eslint/no-misused-promises": [
+      "error",
+      { checksVoidReturn: { attributes: false } },
+    ],
+    "@typescript-eslint/no-unsafe-assignment": 0,
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
+import eslint from "vite-plugin-eslint";
 
 export default defineConfig({
+  plugins: [eslint()],
   server: {
     port: 3000,
     open: true,
