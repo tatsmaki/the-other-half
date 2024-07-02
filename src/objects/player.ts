34f3908bf9381902d765f2761f2f789f8bd75cb7
Rostislav Harlanov <rastsislau.kharlanau@innowise-group.com>
2024-07-02 18:17:38 +0200

    feat: animate run sprite

--- src/objects/player.ts
+++ src/objects/player.ts
@@ -5,41 +5,21 @@ import {
   MeshBasicMaterial,
   PlaneGeometry,
   Quaternion,
-  RepeatWrapping,
-  Texture,
-  TextureLoader,
   Vector3,
 } from "three";
 import { createArrowHelper } from "./arrow_helper";
-
-const idle = (texture: Texture, time: number) => {
-  const th = 4;
-  const tv = 3;
-  const total = th * tv;
-  const tile = Math.ceil((time / 160) % total);
-  const x = Math.ceil(tile / th);
-  const y = tile % tv || tv;
-
-  texture.repeat.set(1 / th, 1 / tv);
-  texture.offset.x = (1 / th) * x;
-  texture.offset.y = (1 / tv) * y;
-};
+import { createIdleTexture, createRunTexture } from "./player_textures";
 
 const quaternion = new Quaternion();
 const yAxis = new Vector3(0, 1, 0);
 
 export const createPlayer = () => {
-  const texture = new TextureLoader().load("Idle.png");
-
-  texture.anisotropy = 4;
-  texture.wrapS = RepeatWrapping;
-  texture.wrapT = RepeatWrapping;
-  texture.center.x -= 0.01;
-  texture.center.y -= 0.02;
+  const idle = createIdleTexture();
+  const run = createRunTexture();
 
   const material = new MeshBasicMaterial({
     side: FrontSide,
-    map: texture,
+    map: idle.texture,
     color: 0xe9724f,
     transparent: true,
     precision: "highp",
@@ -59,15 +39,17 @@ export const createPlayer = () => {
     group,
     render(direction: Vector3, time: number) {
       if (direction.length()) {
+        material.map = run.texture;
+        run.render(time);
         group.position.add(direction);
         arrowHelper.render(direction);
         quaternion.setFromUnitVectors(yAxis, direction.clone().normalize());
+      } else {
+        material.map = idle.texture;
+        idle.render(time);
       }
 
-      // mesh.rotation.setFromQuaternion(quaternion);
       mesh.quaternion.slerp(quaternion, 0.1);
-
-      idle(texture, time);
     },
   };
 };
