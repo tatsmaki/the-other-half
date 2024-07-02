c677fbd81bec0d47359ffd3070d2f474b81203f1
Rostislav Harlanov <rastsislau.kharlanau@innowise-group.com>
2024-07-02 20:13:20 +0200

    chore: speed insights

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
+import { injectSpeedInsights } from "@vercel/speed-insights";
 import { GameScreen } from "./screens/game/game";
 // import { LoadingScreen } from "./screens/loading";
 import "./style.css";
+
+injectSpeedInsights();
 
 // LoadingScreen();
 
