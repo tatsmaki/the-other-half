1ef6a9c2ba1bd4b8a29fad3c9dd503549b42c050
Rostislav Harlanov <rastsislau.kharlanau@innowise-group.com>
2024-07-18 15:48:56 +0200

    chore: remove vercel speed insights

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
-import { injectSpeedInsights } from "@vercel/speed-insights";
 import { GameScreen } from "./screens/game";
 // import { LoadingScreen } from "./screens/loading";
 import "./style.css";
-
-injectSpeedInsights({ debug: false });
 
 // LoadingScreen();
 
